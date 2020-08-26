93d1c26175d373e965ecbfdf80e5fb9e8f44e449
usman <usmanwahyuhp@gmail.com>
2020-08-26 13:03:31 +0700

    all files push

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,13 @@
-const CACHE_NAME = "championleague-v2";
+const CACHE_NAME = "championleague-v4";
 var urlsToCache = [
   "/",
   "/assets/champion.png",
+  "/assets/icon-512x512.png",
+  "/assets/icon-192x192.png",
+  "/assets/icon-96x96.png",
   "/nav.html",
   "/index.html",
+  "/manifest.json",
   "/pages/team.html",
   "/pages/classement.html",
   "/pages/score.html",
@@ -61,5 +65,26 @@ self.addEventListener("activate", function (event) {
         })
       );
     })
+  );
+});
+
+self.addEventListener("push", function (event) {
+  var body;
+  if (event.data) {
+    body = event.data.text();
+  } else {
+    body = "Push message no payload";
+  }
+  var options = {
+    body: body,
+    icon: "/assets/icon-96x96.png",
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: 1,
+    },
+  };
+  event.waitUntil(
+    self.registration.showNotification("Push Notification", options)
   );
 });
